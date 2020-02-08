85673deb6ffe97be70dd6967ba754583d28c5994
Mark Price <pricehome@gmail.com>
2020-02-08 16:19:40 -0600

    html

---
+++ script.js
@@ -0,0 +1,14 @@
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
+
+// Play & pause video
+function toggleVideoStatus() {
+	if (video.paused) {
+		video.play()
+	} else {
+		video.pause()
+	}
+}
